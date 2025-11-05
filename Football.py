c1f33622f08b3ce95c289356d9e792964caf9132
Abhay Pratap Singh <singh071986@gmail.com>
2025-11-04 21:17:35 -0500

    FootbalMiniproject

---
+++ Football.py
@@ -0,0 +1,43 @@
+class Football:
+
+    def __init__(selfs):
+        pass
+
+    def football(self):
+        #return "This is the Football module."
+        import pandas as pd
+        from datetime import datetime
+        #step2
+        df=pd.read_csv('/Users/Yuvaan/Downloads/results.csv')
+        print(f"count before  dropping missing value ::\n{df.shape[0]}")
+        if df.isna().sum().sum()>0:
+           print(f"Missing value count row wise::\n{df.isna().sum().sum()}")
+           print(f"Drop if any missing value::\n{df.dropna()}")
+        print(f"count after drop missing value ::\n{df.shape[0]}")
+        #step3
+        print(f"No of tuples/rows ::{df.shape[0]}")
+        print(f"Number of tournaments in dataset::\n {df['tournament'].nunique()}")
+        #step4
+        #convert date column  in to timestamp format
+        df['date']=pd.to_datetime(df['date'],errors='coerce')
+        #read year 2018 matches using timestamp
+        print(f"Number of matches played in 2018 using timestamp::\n {df[df['date'].dt.year==2018].shape[0]}")
+
+        #convert date column back to string format #read year 2018 matches using string
+        df['date']=pd.to_datetime(df['date'],errors='coerce').dt.strftime('%Y-%m-%d')
+        print(f"Number of matches played in 2018 using string ::\n {df[df['date'].str.contains('2018')].shape[0]}")
+
+        #step5 Calculate how many times the home team won, lost, or had a draw.
+        home_wins=df[df['home_score']>df['away_score']].shape[0]
+        away_wins=df[df['home_score']<df['away_score']].shape[0]
+        draws=df[df['home_score']==df['away_score']].shape[0]
+        print(f"Home team wins: {home_wins}")
+        print(f"Away team wins: {away_wins}")
+        print(f"Draws: {draws}")
+
+    #step 6
+
+fb=Football()
+fb.football()
+
+
