0c54eb06161b9fe0b448f1604ceeb5179b5c9072
Abhay Pratap Singh <singh071986@gmail.com>
2025-11-05 19:03:42 -0500

    FootbalMiniproject

--- Football.py
+++ Football.py
@@ -28,8 +28,8 @@ class Football:
         print(f"Number of matches played in 2018 using string ::\n {df[df['date'].str.contains('2018')].shape[0]}")
 
         #step5 Calculate how many times the home team won, lost, or had a draw.
-        #home_wins=df[df['home_score']>df['away_score']].shape[0] # or (id
-        home_wins=(df['home_score'] > df['away_score']).sum()
+        home_wins=df[df['home_score']>df['away_score']].shape[0] # or (id
+        #home_wins=(df['home_score'] > df['away_score']).sum()
         away_wins= df[df['home_score']<df['away_score']].shape[0]
         draws=df[df['home_score']==df['away_score']].shape[0]
         print(f"Home team wins: {home_wins}")
